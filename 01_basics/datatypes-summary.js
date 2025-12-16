e0af49fdc1e440278aaac3ad17e1abc7f3375ca2
Nandini Mehra <mnandani035@gmail.com>
2025-12-16 11:27:03 +0000

    datatypesummary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+//Primitive 
+
+// 7 types : String, number , Boolean, null, underfined , symbol, BigInt
+
+const score = 100 
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 65757222655522223334n
+
+// Non Primitive (Referece)
+
+//Array , Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]//array
+let myObj={
+    name: "nandini",//objects (key value pair)
+    age: 20
+}
+
+//function 
+const myfunction= function(){
+    console.log("hello! nandini");
+}
+
+console.log(typeof outsideTemp);
