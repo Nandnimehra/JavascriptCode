3c3a510054b3a5e4a12240c4d5141000b1709715
Nandini Mehra <mnandani035@gmail.com>
2025-12-16 05:21:35 +0000

    datat

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,22 @@
+const accountId = 144453
+let accountEmail = "nandani035@gmail.com"
+var accountPassword = "12345"
+accountCity = "Itarsi" // bina variable ke define. but not use
+
+let accountState;//undefined
+
+//accountId = 2 // not allowed
+
+accountEmail = "mnandani035@gmail.com"
+accountPassword = "231367"
+accountCity = "jaipur"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState])
+
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope 
+*/
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+
+// //alert("hello") we are using node.js ,not browser
+
+console.log(3 
+    +3) //code readability should be high
+
+console.log("Nandini");
+
+let name = "Nandini"
+let age = 19
+let isLoggedIn = false
+
+//premitive datatype
+// number => 2 to power 53
+//bigint 
+//string => ""
+// boolean => true /false
+// null =>standalone value
+//undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object
+
+
+
+
