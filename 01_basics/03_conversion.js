fbd66627ed069aa8a2e30ab082b97c8894f4ce7b
Nandini Mehra <mnandani035@gmail.com>
2025-12-16 06:12:38 +0000

    conversion

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,76 +1,76 @@
-let score = null
-// let score = undefined
-// let score = "33abc"
-// let score = true
+// let score = null
+// // let score = undefined
+// // let score = "33abc"
+// // let score = true
 
 
-// console.log(typeof score);
-// console.log(typeof(score));
+// // console.log(typeof score);
+// // console.log(typeof(score));
 
-let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
-
-
-// "33" => 33
-// " 33abc" => NaN
-// true => 1; false => 0
+// let valueInNumber = Number(score)
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber);
 
 
-let isLoggedIn = 1
-// let isLoggedIn = "Nandini"
-
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
-
-//1 => true; 0=> false
-// "" => false 
-// "Nandini" => true
-
-let someNumber = 33
-
-let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+// // "33" => 33
+// // " 33abc" => NaN
+// // true => 1; false => 0
 
 
-// ************************* OPERATIONS *******************************
+// let isLoggedIn = 1
+// // let isLoggedIn = "Nandini"
 
-let value = 3 
-let negValue = -value
-// console.log(negValue);
+// // let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn);
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2/3);
-// console.log(2%3);
+// //1 => true; 0=> false
+// // "" => false 
+// // "Nandini" => true
 
+// let someNumber = 33
 
-let str1 = "hello"
-let str2 = " Nandini"
-
-let str3 = str1 + str2
-// console.log(str3);
-
-// console.log("1" +2);//12
-// console.log(1 + "2");//12
-// console.log("1" + "2");//12
-// console.log("1" + 2 + 2);//122
-// console.log(2 + 2 + "1");//41 //do not use this types of code 
-
-// console.log(+true);//prefer readability
-// console.log(+"");//not use
+// let stringNumber = String(someNumber)
+// // console.log(stringNumber);
+// // console.log(typeof stringNumber);
 
 
-let gameCounter = 100
-++gameCounter;
-console.log(gameCounter);
+// // ************************* OPERATIONS *******************************
+
+// let value = 3 
+// let negValue = -value
+// // console.log(negValue);
+
+// // console.log(2+2);
+// // console.log(2-2);
+// // console.log(2*2);
+// // console.log(2**3);
+// // console.log(2/3);
+// // console.log(2%3);
+
+
+// let str1 = "hello"
+// let str2 = " Nandini"
+
+// let str3 = str1 + str2
+// // console.log(str3);
+
+// // console.log("1" +2);//12
+// // console.log(1 + "2");//12
+// // console.log("1" + "2");//12
+// // console.log("1" + 2 + 2);//122
+// // console.log(2 + 2 + "1");//41 //do not use this types of code 
+
+// // console.log(+true);//prefer readability
+// // console.log(+"");//not use
+
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
 
 
 
-
+//https://tc39.es/ecma262/#sec-syntax-directed-operations
 
 
 
