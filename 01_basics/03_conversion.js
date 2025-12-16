9e9c3c3d3ca294c20cc94e5b20ce389b3823f7b4
Nandini Mehra <mnandani035@gmail.com>
2025-12-16 05:49:13 +0000

    conversion of datatype

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,39 @@
+let score = null
+// let score = undefined
+// let score = "33abc"
+// let score = true
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// " 33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = 1
+// let isLoggedIn = "Nandini"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0=> false
+// "" => false 
+// "Nandini" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
