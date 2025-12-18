e4dfbb569d2ea8a3b6a47af549392cc7d2180708
Nandini Mehra <mnandani035@gmail.com>
2025-12-18 13:47:04 +0000

    object

--- 03_objects/02_objects.js
+++ 03_objects/02_objects.js
@@ -22,12 +22,12 @@ const regularUser ={
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 
-// const obj3 = {obj1, obj2}
-// const obj3 =Object.assign(obj1, obj2)
-//const obj3 =Object.assign({} ,obj1, obj2)
+const obj3 = {obj1, obj2}
+const obj3 =Object.assign(obj1, obj2)
+const obj3 =Object.assign({} ,obj1, obj2)
 
 const obj3 = {...obj1, ...obj2}
-console.log(obj3);
+//console.log(obj3);
 
 //fatabase value
 const users = [{
@@ -43,10 +43,46 @@ email: "nandinimehra.com"
 users[1].email
 console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser)); //output value => datatype array
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+console.log(Object.keys(tinderUser)); //output value => datatype array
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
 
 console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
+// dstructure {}
+
+const course = {
+    coursename: "js with me",
+    price: "999",
+    courseInstructor: "Nandini"
+}
+
+//course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
+
+//in react 
+
+const navbar = ({company}) => {
+
+}
+
+navbar(company="Nandini")
+
+
+//API basics(value => json)
+
+// {
+//    "name": "nandini",
+//    "coursename": "js with me",
+//    "price": "free" 
+// }
+ 
+[
+    {},
+    {},
+    {}
+]
