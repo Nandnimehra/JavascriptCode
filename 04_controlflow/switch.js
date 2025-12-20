00b9f64f2b7631b44c0ce39d41190ea5f6b5fa39
Nandini Mehra <mnandani035@gmail.com>
2025-12-20 17:23:43 +0000

    switch

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,30 @@
+// switch 
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const months = 3
+
+switch (months) {
+    case 1:
+        console.log("January");  
+        break;
+    case 2:
+        console.log("feb");  
+        break;
+    case 1:
+        console.log("march");  
+        break;
+    case 1:
+        console.log("april");  
+        break;
+
+    default:
+        console.log("default cash match ");
+        break;
+}
