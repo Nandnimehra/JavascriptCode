312494863571f57c6d6a15138ed397ba3b76344b
Nandini Mehra <mnandani035@gmail.com>
2025-12-22 05:45:50 +0000

    filter

--- 05_loops/6.js
+++ 05_loops/6.js
@@ -13,14 +13,14 @@
 //2
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10]
 
-//const newNums = myNums.filter( (num) => num > 4 )
+const newNums = myNums.filter( (num) => num > 4 )
 //const newNums = myNums.filter( (num) => {
     //return num > 4 // (scope use) return likhna must h in thid type code
 // })
 //console.log(newNums);
 
 
-const newNums =[]
+//const newNums =[]
 
 myNums.forEach( (num) => {
     if (num > 4) {
@@ -54,5 +54,5 @@ userBooks = books.filter( (bk) => {
 }
 )
 
-console.log(userBooks);
+//console.log(userBooks);
 
