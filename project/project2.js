d93b6ac77e1db8ed6b665c6bd9401efee0365380
Nandini Mehra <mnandani035@gmail.com>
2025-12-25 04:34:22 +0000

    project

--- project/project2.js
+++ project/project2.js
@@ -19,6 +19,32 @@ if(height === '' || height < 0 || isNaN(height)){
   results.innerHTML="please give a valid weight"
 } else {
   const bmi =(weight / ((height*height/10000)).toFixed(2))
+  // show the results 
+  results.innerHTML = `<span>${bmi}</span>`
+}
+const bmiGuide =document.querySelector('#bmiGuide') 
+if(weight <= 18.6){
+  results.innerHTML="under weight"
+
+}else if(weight === 18.6 || 24.9){
+  results.innerHTML="Normal weight"
+}else{
+  results.innerHTML="overweight"
+
 }
 
+
 })
+
+// project 3
+
+const clock =document.getElementById('clock');
+//const clock =document.querySelector('#clock')
+
+
+
+setInterval(function(){
+  let date = new Date();
+  //console.log(date.toLocaleString());
+  clock.innerHTML=date.toLocaleTimeString();
+}, 1000)
