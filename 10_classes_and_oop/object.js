b6e499eb8dface064d89d6206efa2cc9d7b054b9
Nandini Mehra <mnandani035@gmail.com>
2025-12-27 07:25:53 +0000

    object

--- 10_classes_and_oop/object.js
+++ 10_classes_and_oop/object.js
@@ -7,6 +7,22 @@ console.log(multipleBy5(5));
 console.log(multipleBy5.power);
 console.log(multipleBy5.prototype);
 
-function creatUser(username, score){
+function createUser(username, score){
     this.username = username
+    this.score = score
 }
+
+createUser.prototype.increment = function(){
+    this.score++
+}
+createUser.prototype.printMe = function(){
+    console.log(`price is ${this.score}`);
+    
+}
+
+const chai = new createUser("chai",25)
+const tea = createUser("tea",250)
+
+chai.printMe()
+
+// this means => jisne bhi bulaya ho
